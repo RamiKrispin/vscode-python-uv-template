32d607fbbe7392f759c50b2d7b274319b355cdd6
RamiKrispin <ramkrisp@umich.edu>
2025-06-20 12:40:34 -0700

    updated the docker settings

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -3,11 +3,11 @@
 # Image settings
 user_name=rkrispin
 image_label=python-dev
-image_tag=0.2.1
-quarto_ver="1.6.39"
+image_tag=0.2.2
+quarto_ver="1.7.32"
 python_ver=3.11
 venv_name="python-$python_ver-dev"
-ruff_ver="0.8.4"
+ruff_ver="0.12.0"
 dockerfile="Dockerfile"
 # Identify the CPU type (M1 vs Intel)
 if [[ $(uname -m) ==  "aarch64" ]] ; then
@@ -31,8 +31,6 @@ docker build . -f $dockerfile \
                 --build-arg VENV_NAME=$venv_name \
                 --build-arg PYTHON_VER=$python_ver \
                 --build-arg RUFF_VER=$ruff_ver \
-                --build-arg GIT_USER_NAME=$GIT_USER_NAME \
-                --build-arg GIT_USER_EMAIL=$GIT_USER_EMAIL \
                 -t $image_name
 
 if [[ $? = 0 ]] ; then
--- docker/install_dependencies.sh
+++ docker/install_dependencies.sh
@@ -15,6 +15,11 @@ apt-get update && apt-get install -y --no-install-recommends \
    apt-utils \
    locales \
    fzf \
+   btop \
+   bat \
+   unzip \
+   fontconfig \
+   software-properties-common \
    && rm -rf /var/lib/apt/lists/*
 
 # Installing oh my zsh
@@ -30,10 +35,37 @@ chsh -s $(which zsh)
 # !Installing colorls
 # * Required locales
 gem install colorls
-echo "alias ls='colorls -lA --sd'" >> ${ZDOTDIR:-$HOME}/.zshrc 
+echo "alias lc='colorls -lA --sd'" >> ${ZDOTDIR:-$HOME}/.zshrc 
 locale-gen en_US.UTF-8
 
-# TODO
-# ! Check why it install old fzf version 
-# ? Check if to install using git clone
-# echo eval "$(fzf --zsh)" >> ${ZDOTDIR:-$HOME}/.zshrc 
+# Set the bindkey options
+echo "bindkey '^n' history-search-forward" >> ${ZDOTDIR:-$HOME}/.zshrc 
+echo "bindkey '^p' history-search-backward" >> ${ZDOTDIR:-$HOME}/.zshrc 
+echo "bindkey -e" >> ${ZDOTDIR:-$HOME}/.zshrc 
+
+# Set the completion styling
+# case sensitive - show upper case results when using lower case
+echo "zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'" >> ${ZDOTDIR:-$HOME}/.zshrc 
+echo "zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"" >> ${ZDOTDIR:-$HOME}/.zshrc 
+
+# Setting the zsh history
+echo "HISTSIZE=5000" >> ${ZDOTDIR:-$HOME}/.zshrc 
+echo "HISTFILE=${ZDOTDIR:-$HOME}/.zsh_history" >> ${ZDOTDIR:-$HOME}/.zshrc 
+echo "SAVEHIST=$HISTSIZE" >> ${ZDOTDIR:-$HOME}/.zshrc 
+echo "HISTDUP=erase" >> ${ZDOTDIR:-$HOME}/.zshrc 
+echo "setopt appendhistory" >> ${ZDOTDIR:-$HOME}/.zshrc
+echo "setopt sharehistory" >> ${ZDOTDIR:-$HOME}/.zshrc
+echo "setopt hist_ignore_all_dups" >> ${ZDOTDIR:-$HOME}/.zshrc
+echo "setopt hist_save_no_dups" >> ${ZDOTDIR:-$HOME}/.zshrc
+echo "setopt hist_ignore_dups" >> ${ZDOTDIR:-$HOME}/.zshrc
+echo "alias c=clear" >> ${ZDOTDIR:-$HOME}/.zshrc
+
+
+
+
+
+# Adding fastfetch
+add-apt-repository -y ppa:zhangsongcui3371/fastfetch
+apt-get update 
+apt-get install fastfetch
+echo "fastfetch" >> ${ZDOTDIR:-$HOME}/.zshrc
